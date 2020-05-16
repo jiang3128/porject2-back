a4070ee81610d477bdde228767de5a0cca4c3bac
jiang3128 <zhuj3128@uwec.edu>
2020-05-16 03:27:59 +0000

    back end

---
+++ make.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS today;
+DROP USER IF EXISTS  today_user@localhost;
+
+CREATE DATABASE today CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER today_user@localhost IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON today.* TO today_user@localhost;
+
